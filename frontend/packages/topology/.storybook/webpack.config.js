0997c39e7a85b56ecc10b4727ac6bb6b4c749db1
christianvogt <cvogt@redhat.com>
2019-10-10 22:49:51 -0400

    add scss support to storybook

--- frontend/packages/topology/.storybook/webpack.config.js
+++ frontend/packages/topology/.storybook/webpack.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
 module.exports = ({ config }) => {
   config.module.rules.push({
-
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
+  config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
