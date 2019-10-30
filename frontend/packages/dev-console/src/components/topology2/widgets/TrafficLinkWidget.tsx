a775c6452d38078e93fe49d36bfd6c0215e6a0ba
christianvogt <cvogt@redhat.com>
2019-10-30 17:14:57 -0400

    add traffic percentage

--- frontend/packages/dev-console/src/components/topology2/widgets/TrafficLinkWidget.tsx
+++ frontend/packages/dev-console/src/components/topology2/widgets/TrafficLinkWidget.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { EdgeEntity } from '@console/topology/src/types';
+import widget from '@console/topology/src/widget';
 import ConnectorArrow from '@console/topology/src/arrows/ConnectorArrow';
 import BaseEdgeWidget from './BaseEdgeWidget';
 
@@ -9,10 +10,29 @@ type TrafficLinkWidgetProps = {
   entity: EdgeEntity;
 };
 
-const TrafficLinkWidget: React.FC<TrafficLinkWidgetProps> = ({ entity }) => (
-  <BaseEdgeWidget entity={entity} className="odc-traffic-link">
-    <ConnectorArrow edge={entity} />
-  </BaseEdgeWidget>
-);
+const TrafficLinkWidget: React.FC<TrafficLinkWidgetProps> = ({ entity }) => {
+  const { percent } = entity.getData().data;
+  let text = null;
+  if (percent != null) {
+    const startPoint = entity.getStartPoint();
+    const endPoint = entity.getEndPoint();
+    text = (
+      <text
+        className="odc-traffic-link__text"
+        x={(endPoint.x + startPoint.x) / 2}
+        y={(endPoint.y + startPoint.y) / 2}
+        textAnchor="middle"
+      >
+        {percent}%
+      </text>
+    );
+  }
+  return (
+    <BaseEdgeWidget entity={entity} className="odc-traffic-link">
+      <ConnectorArrow edge={entity} />
+      {text}
+    </BaseEdgeWidget>
+  );
+};
 
-export default TrafficLinkWidget;
+export default widget(TrafficLinkWidget);
